88327a26204af85640d4907e7f9e97586ca8aadb
minjong <koust6u@pusan.ac.kr>
2023-12-08 22:02:26 +0900

    테스트

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        axios.post('http://localhost:8080/member/login',
+        axios.post('http://127.0.0.1:8080/member/login',
             JSON.stringify({ userId, password }),
             {
                 headers: {
